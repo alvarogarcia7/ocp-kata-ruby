fc149f9d10816a729a700a1f6a1be0ad6ba4d7d5
Carlos Ble+Alvaro Garcia <derecerca@gmail.com>
2015-10-19 19:50:26 +0200

    green

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,10 @@
 RSpec.describe "#say" do
   it "simple numbers are converted to text" do
     expect(say(1)).to eq "1"
+    expect(say(2)).to eq "2"
   end
 end
 
 def say a_number
-  "1"
+  a_number.to_s
 end
